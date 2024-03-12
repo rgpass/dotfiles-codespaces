4fda0b7d10755567d9d1fad3dc2ac488ac389ddc
Gerry Pass <rgpass@gmail.com>
2024-03-12 09:49:58 -0400

    Set default shell to zsh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Copy zsh aliases
 cp /workspaces/.codespaces/.persistedshare/dotfiles/aliases.zsh /home/vscode/.oh-my-zsh/custom/aliases.zsh
+
+# Set default shell to zsh
+sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
 
 zsh
