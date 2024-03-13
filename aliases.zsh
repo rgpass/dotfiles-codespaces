8e5c044345c104f7e00b252d2ba4cd4f6b7d9f38
Gerry Pass <rgpass@gmail.com>
2024-03-13 08:56:58 -0400

    Not sure why the semicolon is causing an issue

--- aliases.zsh
+++ aliases.zsh
@@ -1,11 +1,11 @@
 alias ff="bin/toggle-feature-flag enable copilot-custom-models"
 alias os="script/setup-codespaces-orca"
 alias or="cd /workspaces/orca ; script/bootstrap; script/run;"
-alias orca="os ; or"
+alias orca="os && or"
 alias co="script/setup-copilot"
 alias rs="script/server"
 
-alias rss="ff ; co"
+alias rss="ff && co"
 alias rsr="rs"
 
 alias u="echo cfbbiz0org1-admin"
