f6f9ae0ebc4df49ccd000fe972cbb9df73c12aa8
Gerry Pass <rgpass@gmail.com>
2024-03-13 11:19:05 -0400

    Troubleshooting codespaces not working

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@
 cp /workspaces/.codespaces/.persistedshare/dotfiles/aliases.zsh /home/vscode/.oh-my-zsh/custom/aliases.zsh
 
 # Set default shell to zsh
-sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
+# sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
 
 zsh
