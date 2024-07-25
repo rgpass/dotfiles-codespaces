a1afe3c19a53e74bfc74d35c57c17b514869cbb0
Gerry Pass <rgpass@gmail.com>
2024-07-24 22:09:34 -0400

    Add new aliases for working in dotcom

--- aliases.zsh
+++ aliases.zsh
@@ -10,3 +10,11 @@ alias rss="ff && co"
 alias rsr="rs"
 
 alias u="echo cfbbiz0org1-admin"
+alias repos="sudo chmod +t /tmp"
+
+alias minitest="bin/rails test packages/orca/test/"
+
+fix_path() {
+  # Run when npm not found
+  export PATH="/workspaces/github/vendor/node:$PATH:/workspaces/insights-dataplatform/.dotnet:/workspaces/actions/actions-dotnet/.dotnet:/workspaces/github/bin:/workspaces/actions/actions-codespaces/script"
+}
